0ae24ccbff05b63ceead7c5a9a6545786fbc11c5
Moeen Basra <m.basra@live.com>
2018-05-29 00:42:29 +0400

    update stubs

--- src/Foundation/Eloquent/Builder.php
+++ src/Foundation/Eloquent/Builder.php
@@ -7,37 +7,4 @@ use Illuminate\Pagination\Paginator;
 
 class Builder extends \Illuminate\Database\Eloquent\Builder
 {
-    /**
-     * Paginate the given query.
-     *
-     * @param  int $perPage
-     * @param  array $columns
-     * @param  string $pageName
-     * @param  int|null $page
-     * @param  string $dataKey
-     *
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
-     *
-     * @throws \InvalidArgumentException
-     */
-    public function paginate(
-        $perPage = null,
-        $columns = ['*'],
-        $pageName = 'page',
-        $page = null,
-        $dataKey = 'data'
-    )
-    {
-        $page = $page ?: Paginator::resolveCurrentPage($pageName);
-
-        $perPage = $perPage ?: $this->model->getPerPage();
-
-        $results = ($total = $this->toBase()->getCountForPagination()) ? $this->forPage($page, $perPage)->get($columns) : $this->model->newCollection();
-
-        return $this->paginate($results, $total, $perPage, $page, [
-            'path' => Paginator::resolveCurrentPath(),
-            'pageName' => $pageName,
-            'data_key' => $dataKey,
-        ]);
-    }
 }
