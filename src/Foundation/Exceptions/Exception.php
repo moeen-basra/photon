28d93bc7b015b22c77ca5de8dab5a3e5edffb2d5
Moeen Basra <m.basra@live.com>
2019-03-18 14:59:51 +0400

    fix exception handler

--- src/Foundation/Exceptions/Exception.php
+++ src/Foundation/Exceptions/Exception.php
@@ -2,16 +2,10 @@
 
 namespace Photon\Foundation\Exceptions;
 
-use Illuminate\Validation\Validator;
-
 class Exception extends \Exception
 {
-    public function __construct(?$message = "", ?int $code = 400, ?\Throwable $previous = null)
+    public function __construct($message = "", ?int $code = 400, ?\Throwable $previous = null)
     {
-        if ($message instanceof Validator) {
-            $message = \json_encode(\array_merge($message->messages()->toArray(), ['PhotonInputException' => true]));
-        }
-
         parent::__construct($message, $code, $previous);
     }
 }
