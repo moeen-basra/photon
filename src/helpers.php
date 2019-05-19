5d13cfc6b04ec4e2860756e9f1d17a5fbf6ee1df
Moeen Basra <m.basra@live.com>
2019-05-19 23:11:02 +0400

    update helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,67 +1,46 @@
 <?php
-if (! function_exists('public_path')) {
-    /**
-     * Get the path to the public folder.
-     *
-     * @param  string  $path
-     * @return string
-     */
-    function public_path($path = '')
-    {
-        return rtrim(app()->basePath('public/'.$path), DIRECTORY_SEPARATOR);
-    }
-}
-
-
-if ( ! function_exists('config_path'))
-{
+if (!function_exists('config_path')) {
     /**
      * Get the configuration path.
      *
-     * @param  string $path
+     * @param string $path
+     *
      * @return string
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
     function config_path($path = '')
     {
-        return app()->basePath() . '/config' . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+        return app()->make('path.config') . ($path ? DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR) : $path);
     }
 }
 
-if (! function_exists('public_path')) {
+if (!function_exists('public_path')) {
     /**
      * Get the path to the public folder.
      *
-     * @param  string  $path
+     * @param string $path
+     *
      * @return string
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
     function public_path($path = '')
     {
-        return app()->make('path.public').($path ? DIRECTORY_SEPARATOR.ltrim($path, DIRECTORY_SEPARATOR) : $path);
+        return app()->make('path.public') . ($path ? DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR) : $path);
     }
 }
 
-if (! function_exists('bcrypt')) {
+if (!function_exists('bcrypt')) {
     /**
      * Hash the given value.
      *
-     * @param  string  $value
-     * @param  array   $options
+     * @param string $value
+     * @param array $options
+     *
      * @return string
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
-    function bcrypt($value, $options = [])
+    function bcrypt(string $value, $options = []): string
     {
         return app('hash')->make($value, $options);
-    }
-}
-
-if (!function_exists('isNotLumen')) {
-    /**
-     * check if app is not lumen
-     *
-     * @return bool
-     */
-    function isNotLumen() : bool
-    {
-        return ! preg_match('/lumen/i', app()->version());
     }
 }
