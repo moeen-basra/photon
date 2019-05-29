3ee6c67ce7629990ca1ec77df88b93771c082718
Moeen Basra <m.basra@live.com>
2019-05-30 03:56:35 +0400

    implement traits

--- src/Foundation/Eloquent/Model.php
+++ src/Foundation/Eloquent/Model.php
@@ -2,17 +2,9 @@
 
 namespace Photon\Foundation\Eloquent;
 
+use Photon\Foundation\Traits\ModelTrait;
+
 class Model extends \Illuminate\Database\Eloquent\Model
 {
-    public function newEloquentBuilder($query): Builder
-    {
-        return new Builder($query);
-    }
-
-    protected function paginateResult($limit, $dataKey, $results)
-    {
-        $limit = is_null($limit) ? config('pagination.limit', 20) : $limit;
-
-        return $results->paginate($limit, ['*'], $pageName = 'page', $page = null, $dataKey);
-    }
+    use ModelTrait;
 }
